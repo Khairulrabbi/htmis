6ef33d82ef2e5a1bd9534b81ffe56db0ded0194a
Khairulrabbi <khairulcse0026@gmail.com>
2016-04-16 14:53:26 +0600

    Validation rules applied

--- app/models/User.php
+++ app/models/User.php
@@ -3,12 +3,25 @@
 namespace App\models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Validator;
 
 class User extends Model
 {
     protected $table = 'users';
     //
     protected $fillable = ['name', 'email', 'password', 'address', 'role_id'];
+
+    public static function validate($input) {
+    	$rules = array(
+            'name' => 'required|alpha|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'address' => 'required|max:255',
+            'role_id' => 'required|numeric|max:11',
+            'password' => 'required|confirmed|min:6',
+    		);
+
+    	return Validator::make($input, $rules);
+    }
 
     public function role() {
     	return $this->belongsToMany('App\models\Role');
