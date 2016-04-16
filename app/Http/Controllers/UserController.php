92887f5f7f6ea935d936cf34a46eddbbc87e2ac2
Khairulrabbi <khairulcse0026@gmail.com>
2016-04-16 14:55:58 +0600

    User Data Saved to DataBase

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,18 +5,55 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
-
 use App\models\Role;
+use App\models\User;
+
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Validator;
+
+use Response;
+use Session;
+
 
 class UserController extends Controller
 {
+	protected $user;
+
+	public function __construct(User $user) {
+		$this->user = $user;
+	}
     public function add() {
     	$roles = Role::all();
     	return view('auth.register', compact('roles'));
     }
 
-    public function saveOrUpdate() {
-    	echo "string";
+    public function saveOrUpdate(Request $request, $id='') {
+    	$user = new User();
+        $v = User::validate(Input::all());
+        if ($v->passes()) {
+            if ($id != '') {
+            
+            } else {
+                $user->name = $request->name;
+                $user->email = $request->email;
+                $user->password = bcrypt($request->password);
+                $user->address = $request->address;
+                $user->role_id = $request->role_id;
+
+                $user->save();
+
+                Session::flash('message', 'User Created Successfully');
+
+                return view('home');
+                    
+            }
+        } else {
+
+            return Redirect::to('user/register')->withErrors($v->getMessageBag());
+        }
+
+
     }
 
 }
