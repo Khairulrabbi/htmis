32c55630137ba636dd665971a1b676dac4d5041b
Khairulrabbi <khairulcse0026@gmail.com>
2016-04-23 11:55:25 +0600

    Edit form design

--- resources/views/auth/edit.blade.php
+++ resources/views/auth/edit.blade.php
@@ -1,116 +1,63 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Update User</div>
-                <div class="panel-body">
-                    <!-- <form class="form-horizontal" role="form" method="POST" action="{{ 'update' }}"> -->
-                    {{ Form::open(['url'=>'user/update/'.$user->id, 'method'=>'post', 'role'=>'form']) }}
 
-                        {!! csrf_field() !!}
+  <div class="register-box">
+  <div class="register-logo">
+    <a href="#"><b>Edit User</b></a>
+  </div>
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Name</label>
+  <div class="register-box-body">
+    <p class="login-box-msg">Edit User Information</p>
+       @include('common.errors')
+    {{ Form::open(['url'=>'user/update/'.$user->id, 'method'=>'post', 'role'=>'form']) }}
+     {!! csrf_field() !!}
+      <div class="form-group has-feedback">
+        <input type="text" class="form-control" value="{{ $user->name }}">
+        <span class="glyphicon glyphicon-user form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="email" class="form-control" value="{{ $user->email }}">
+        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="password" class="form-control" value="{{ $user->password }}"">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="password" class="form-control" value="{{ $user->password }}">
+        <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+          <input type="text" class="form-control" value="{{ $user->address }}" >
+          <span class="glyphicon glyphicon-user form-control-feedback"></span> 
+      </div>
+      <div class="form-group">
+        <select class="form-control select2" style="width: 100%;">
+            <option value="">
+                Choose Role Type
+            </option>
+            @foreach($roles as $role) 
+                <option value="{{$role->id}}">{{$role->name}}</option>
+            @endforeach        
+        </select>
+      </div>
+      <div class="row">
+        <div class="col-xs-8">
+          <div class="checkbox icheck">
+      <!--       <label>
+              <input type="checkbox"> I agree to the <a href="#">terms</a>
+            </label> -->
+          </div>
+        </div><!-- /.col -->
+        <div class="col-xs-4">
+          <button type="submit" class="btn btn-primary btn-block btn-flat">Edit</button>
+        </div><!-- /.col -->
+      </div>
+   {{ Form::close() }}
+  </div><!-- /.form-box -->
+  </div><!-- /.register-box -->
 
-                            <div class="col-md-6">
-                                <input type="text" class="form-control" name="name" value="{{ $user->name }}">
-
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input type="email" class="form-control" name="email" value="{{ $user->email }}">
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input type="password" class="form-control" name="password", value="{{ $user->password }}">
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input type="password" class="form-control" name="password_confirmation">
-
-                                @if ($errors->has('password_confirmation'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-                        <div class="form-group{{ $errors->has('address') ? 'has-error' : ''}}">
-                            <label class="col-md-4 control-label">Address</label>
-
-                            <div class="col-md-6">
-                                <input type="text" class="form-control" name="address" value="{{ $user->address }}"></input>
-                                
-                                @if($errors->has('address'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('address')}}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('role_id') ? 'has-error' : ''}}">
-                            <label class="col-md-4 control-label">Role</label>
-
-                            <div class="col-md-6">
-                                <select class="form-control" name="role_id">
-                                    <option value="">Choose Role Type</option>
-                                        @foreach($roles as $role) 
-                                            <option value="{{$role->id}}">{{$role->name}}</option>
-                                        @endforeach
-                                </select>
-                                
-                                @if($errors->has('role_id'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('role_id')}}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-user"></i>Update
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
+
+
