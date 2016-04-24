2d61462cc978e927bcc918336d3939b0b180e302
Khairulrabbi <khairulcse0026@gmail.com>
2016-04-24 11:59:12 +0600

    Deletion of unnecessary file and final design of access index

--- resources/views/accesses/index.blade.php
+++ resources/views/accesses/index.blade.php
@@ -1,73 +1,80 @@
-@extends('layouts.app')
+@extends('layouts.admin')
 
 @section('content')
-    <div class="container">
-     <!-- Downloading csv data link -->
-    <a href="../csv-data" style="position: absolute; left: 5%; font-size: large;">Download Data</a>
 
+      <!-- Content Wrapper. Contains page content -->
+      <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+          <h1>
+            Data Tables
+            <small>advanced tables</small>
+          </h1>
+          <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li><a href="#">Tables</a></li>
+            <li class="active">Data tables</li>
+          </ol>
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+          <div class="row">
+            <div class="col-xs-12">
+              
+
+              <div class="box">
+                <div class="box-header">
+                  <h3 class="box-title">Data Table With Access List</h3>
+                </div><!-- /.box-header -->
+                <div class="box-body">
+                  <table id="example1" class="table table-bordered table-striped">
+                    <thead>
+                      <tr>
+                        <th>Action Name</th>
+                        <th>Name</th>
+                        <th>Control Name</th>
+                        <th>Delete</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($accesses as $access)
+                        <tr>
+                            <td class="table-text"><div><a href={{ $access->id}}{{'/edit'}}> {{ $access->action_name }}</a></div></td>
+                            <td class="table-text"><div>{{ $access->name }}</div></td>
+                            <td class="table-text"><div>{{ $access->controller_name }}</div></td>
+                            <td>
+                                {!! Form::open(['url'=>'access/'.$access->id, 'files'=>true, 'onsubmit'=>'return ConfirmDelete()']) !!}
+                                    {{ csrf_field() }}
+                                    {{ method_field('DELETE')}}
+                                {!! Form::submit('DELETE',array('class'=>'btn btn-danger')) !!}
+                                {!! Form::close() !!}
+                            </td>
+
+                        </tr>
+                    @endforeach                            
+                    </tbody>
+                  </table>
+                   {{ $accesses->render() }}
+                </div><!-- /.box-body -->
+              </div><!-- /.box -->
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </section><!-- /.content -->
+      </div><!-- /.content-wrapper -->
+     
+
+      <!-- Control Sidebar -->
+      
+      <div class="control-sidebar-bg"></div>
+          <script type="text/javascript">
+              function ConfirmDelete() {
+                  var x = confirm('Are you sure to delete');
+                  if(x)
+                      return true;
+                  else 
+                      return false;
+              }
+          </script>
     
-    <a href="add" style="position: absolute; font-size: large; left:80%;">Add New</a>
-    <a href="../role/list" style="position: absolute; font-size: large; left:15%;">Role List</a>
-        <div class="col-sm-offset-2 col-sm-8">
-                <div class="panel-body">
-                    <!-- Display Validation Errors -->
-                    @include('common.errors')
-                </div>
-
-                <!-- Session Work -->
-            @if (Session::has('message'))
-                 <div class="alert alert-info">
-                    {{ Session::get('message') }}
-                 </div>
-            @endif
-
-            <script type="text/javascript">
-                function ConfirmDelete() {
-                    var x = confirm('Are You sure to Delete');
-                    if (x)
-                        return true;
-                    else 
-                        return false;
-                }
-            </script>
-
-            @if(count($accesses)>0)
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                       Access List
-                    </div>
-
-                    <div class="panel-body">
-                    	<table class="table table-striped task-table">
-                    		<thead>
-                                <th>Action Name</th>
-                                <th>Name</th>
-                                <th>Control Name</th>                   			
-                    		</thead>
-                    		<tbody>
-                    			@foreach($accesses as $access)
-                    				<tr>
-                    					<td class="table-text"><div><a href={{ $access->id}}{{'/edit'}}> {{ $access->action_name }}</a></div></td>
-                    					<td class="table-text"><div>{{ $access->name }}</div></td>
-                    					<td class="table-text"><div>{{ $access->controller_name }}</div></td>
-                    					<td>
-                    						{!! Form::open(['url'=>'access/'.$access->id, 'files'=>true, 'onsubmit'=>'return ConfirmDelete()']) !!}
-                    							{{ csrf_field() }}
-                    							{{ method_field('DELETE')}}
-                    						{!! Form::submit('DELETE',array('class'=>'btn btn-danger')) !!}
-                    						{!! Form::close() !!}
-                    					</td>
-
-                    				</tr>
-                    			@endforeach
-                    		</tbody>
-                    		
-                    	</table>
-                    	{{ $accesses->render() }}
-                    </div>
-                </div>
-            @endif
-        </div>
-    </div>
-
 @endsection
