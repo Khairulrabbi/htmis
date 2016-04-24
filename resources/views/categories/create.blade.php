36487a3c578a73b9c5df3bdc1f1429382c0c678f
Khairulrabbi <khairulcse0026@gmail.com>
2016-04-24 13:21:55 +0600

    Category create done

---
+++ resources/views/categories/create.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.admin')
+
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+              <h1>
+                Advanced Form Elements
+                <small>Preview</small>
+              </h1>
+            <ol class="breadcrumb">
+                <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+                <li><a href="#">Forms</a></li>
+                <li class="active">Create Role</li>
+            </ol>
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+
+            <!-- SELECT2 EXAMPLE -->
+            <div class="box box-default">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Role</h3>
+                    <div class="box-tools pull-right">
+                        <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                        <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-remove"></i></button>
+                    </div>
+                </div><!-- /.box-header -->
+
+                <div class="box-body">
+                @include('common.errors')
+                    {{ Form::open(['url'=>'category/add', 'method'=>'post', 'role'=>'form']) }}
+
+                    {{ csrf_field() }}
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                {{ Form::label('category_name', 'Category Name') }}
+                                {{ Form::text('category_name', '', array('class' => 'form-control', 'placeholder'=>'Enter Category Name'))}} 
+                            </div><!-- /.form-group -->
+
+                            <div class="form-group">
+                                <div class="row">
+                                    <div class="col-xs-8">
+
+                                    </div><!-- /.col -->
+           
+                               <!--      <div class="col-xs-4">
+                                        <a href="../category/list" style="position: absolute;font-size: large;left: 30%">Cancel</a>
+                                    </div> -->
+                                    <div class="col-xs-4">
+                                    <br>
+                                        <button type="submit" class="btn btn-primary btn-block btn-flat">Add New</button>
+                                    </div>
+
+                                    <div class="col-xs-4">
+                                        <a href="../category/list" class="btn btn-primary btn-block btn-flat">Cancel</a> 
+                                    </div>
+                                </div>
+
+                            </div><!-- /.form-group -->
+                        </div><!-- /.col -->
+
+                    </div><!-- /.row -->
+                    {{ Form::close() }}
+                </div><!-- /.box-body -->
+
+            </div><!-- /.box -->
+
+        </section><!-- /.content -->
+    </div><!-- /.content-wrapper -->
+@endsection
